5450a8a5ccb563278d4a46c7b19cead0f0c90c5f
GeronimoJohn <johnageronimo1@gmail.com>
2020-10-25 00:49:06 +1300

    initial setup

---
+++ registration.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+        <h2>Register</h2>
+        </div>
+        <form action="registration.php" method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username">
+            </div>
+
+            <div>
+                <label for="email">Email</label>
+                <input type="text" name="email">
+            </div>
+
+            <div>
+                <label for="password">Password</label>
+                <input type="text" name="Password">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
